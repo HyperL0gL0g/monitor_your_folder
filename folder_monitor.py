abdc52f47125110c3c6d39eeb78b3a6a043d5803
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-05-26 21:13:52 +0530

    Update folder_monitor.py

--- folder_monitor.py
+++ folder_monitor.py
@@ -1,9 +1,9 @@
 import os, time
 path_to_watch = "."
-before = dict ([(f, None) for f in os.listdir ("F:")])
+before = dict ([(f, None) for f in os.listdir ("PATH OF FOLDER YOU WANT TO MONITOR")])
 while 1:
   time.sleep (10)
-  after = dict ([(f, None) for f in os.listdir ("F:")])
+  after = dict ([(f, None) for f in os.listdir ("PATH OF FOLDER YOU WANT TO MONITOR")])
   added = [f for f in after if not f in before]
   removed = [f for f in before if not f in after]
   if added: print("Added: ", ", ".join (added))
